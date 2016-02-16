3d97dcf62cbdc5621c00a42bface4603e0b54c12
Slapyousilly <n00b1ass@gmail.com>
2016-02-16 17:04:31 +0800

    Modified Camera Values and currently commented out Minimaps

--- ComGraphics/Source/Minimap.cpp
+++ ComGraphics/Source/Minimap.cpp
@@ -1,12 +1,15 @@
-#include "Vector3.h"
-#include "Character.h"
-#include "SP2.h"
-
-//void main(void)
+//#include "Vector3.h"
+//#include "Character.h"
+//#include "SP2.h"
+//
+//void testing();
+//
+//void testing()
 //{
 //    Player player;
 //    player.pos.x; //player position
 //
 //    Camera3 minimap;
+//
 //
 //}
